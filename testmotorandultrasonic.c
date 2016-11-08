1be49e5bf6365815f4c1b6e7ab6252ca564c7021
bassixt <mrcclt@hotmail.it>
2016-11-08 22:46:45 +0100

    Update testmotorandultrasonic.c

--- testmotorandultrasonic.c
+++ testmotorandultrasonic.c
@@ -145,7 +145,7 @@ do {
 		set_tacho_ramp_up_sp( med, 0 );
 		set_tacho_ramp_down_sp( med, 0 );
 		set_tacho_position_sp( med, 5 );
-		for ( i = 0; i < 8; i++ ) {
+		for ( i = 0; i < 3; i++ ) {
 			set_tacho_command_inx( med, TACHO_RUN_TO_REL_POS );
 			Sleep( 500 );
 		}
