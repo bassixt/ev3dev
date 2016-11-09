1b72c32aa205e921850d776b1dfb9f14a5e8135a
bassixt <mrcclt@hotmail.it>
2016-11-09 21:59:15 +0100

    Update testmotorandultrasonic.c

--- testmotorandultrasonic.c
+++ testmotorandultrasonic.c
@@ -153,7 +153,8 @@ do {
 	} else {
 		printf( "LEGO_EV3_L_MOTOR 1 is NOT found\n" );
 	} 
-	set_tacho_stop_action_inx( med, 2 );
+	
+	set_tacho_stop_action_inx(med,TACHO_HOLD)
 	//set_tacho_hold_pid_Kd 	(med,1000); 
 	//ev3_stop_command_motor(med,2);
 //Run all sensors
