e75ee19f777686c7b6ce11be9d2b6538dc7f5385
bassixt <mrcclt@hotmail.it>
2016-11-24 20:03:53 +0100

    Update testmotorandultrasonic.c

--- testmotorandultrasonic.c
+++ testmotorandultrasonic.c
@@ -273,8 +273,8 @@ while((finish - beginning - distance)<=0){
                         printf( "compass:(%f) \n", value_compass);
                         fflush( stdout );
 	/*compensate the rotation*/
-	set_tacho_time_sp( sn, 40 );
-	set_tacho_time_sp( dx, 40 );	
+	set_tacho_time_sp( sn, 100 );
+	set_tacho_time_sp( dx, 100 );	
 	if (value_compass > init_compass_value +2 ) //rotate to left
 	{
 	set_tacho_speed_sp( sn, max_speed *1/24 );
