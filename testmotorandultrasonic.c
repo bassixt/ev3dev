c17314eb8e1e606733cc4930fe5d5cded0461377
bassixt <mrcclt@hotmail.it>
2016-11-24 13:12:42 +0100

    Update testmotorandultrasonic.c

--- testmotorandultrasonic.c
+++ testmotorandultrasonic.c
@@ -288,12 +288,12 @@ while((finish - beginning - distance)<=0){
 	set_tacho_speed_sp( sn, max_speed * 1 / 12 );
 	set_tacho_speed_sp( dx, max_speed * 1 / 12 );
 			       }
-	if(value<70 && value >=25)
+	if(value<70 && value >=20)
 		{
 	set_tacho_speed_sp( sn, max_speed * 1 / 24 );
 	set_tacho_speed_sp( dx, max_speed * 1 / 24 );
 		 }
-	if(value<25 && value >=0)
+	if(value<20 && value >=0)
 		 {   	 
 	 set_tacho_speed_sp( sn, max_speed * 0 );
 	 set_tacho_speed_sp( dx, max_speed * 0 );
