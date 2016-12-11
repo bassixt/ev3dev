8981aef4f89b5b7068164f7299fefe0d20687e9a
bassixt <coletta@eurecom.fr>
2016-12-11 18:53:56 +0100

    Update testmotorandultrasonic.c

--- testmotorandultrasonic.c
+++ testmotorandultrasonic.c
@@ -855,7 +855,7 @@ switch(donald->number)
 	
 		rotatesx(donald->sn,donald->dx,donald->sn_compass,donald->max_speed,90,donald->sn_mag);
 		// go around 1m (TO TEST !!! and mesure on the arena)
-		go_ahead_till_obstacle(donald->sn,donald->dx,donald->max_speed,donald->sn_sonar,1121,donald->sn_compass,donald->sn_mag);
+		go_ahead_till_obstacle(donald->sn,donald->dx,donald->max_speed,donald->sn_sonar,1140,donald->sn_compass,donald->sn_mag);
 		//TURN RIGHT 
 		/*final_pos=get_compass_values(donald->sn_compass);
 		if(final_pos>initial_pos)
