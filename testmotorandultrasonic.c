b37f2944a21e138b0cd45537cddddccd88d6befe
bassixt <coletta@eurecom.fr>
2016-12-11 18:07:48 +0100

    Update testmotorandultrasonic.c

--- testmotorandultrasonic.c
+++ testmotorandultrasonic.c
@@ -859,9 +859,9 @@ switch(donald->number)
 		//TURN RIGHT 
 		final_pos=get_compass_values(donald->sn_compass);
 		if(final_pos<initial_pos)
-	   		turn_pos=359-initial_pos+final_pos;
+	   		turn_pos=final_pos-initial_pos;
 		else	
-			turn_pos=final_pos-initial_pos;
+			turn_pos=359-initial_pos+final_pos;
 		rotatedx(donald->sn,donald->dx,donald->sn_compass,donald->max_speed,(int)turn_pos,donald->sn_mag);
 		// go until final base
 		go_ahead_till_obstacle(donald->sn,donald->dx,donald->max_speed,donald->sn_sonar,3230,donald->sn_compass,donald->sn_mag);
