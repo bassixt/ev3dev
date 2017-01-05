a1c342654a880633bd65001714c3a4cebc1b08f0
bassixt <coletta@eurecom.fr>
2017-01-05 22:59:02 +0100

    Update testmotorandultrasonic.c

--- testmotorandultrasonic.c
+++ testmotorandultrasonic.c
@@ -320,25 +320,28 @@ void positioning(uint8_t sn, uint8_t dx, int max_speed, uint8_t sn_mag)
 	   {
 	   new_angle = 0;
 	   }
-	printf("the value is : %f\n",new_angle);
 	new_angs = (short)new_angle;
 	m_rot = - (new_angs - last_angle);
 	m_rot = deg2rad(m_rot);
 	last_angle = new_angs;
-	printf("the angle is: %f\n", m_rot);
+	printf("the angle in rad is: %f\n", m_rot);
 	printf("the angle in deg is:%f\n", rad2deg(m_rot));
 	get_tacho_position(sn,&new_sx);
 	get_tacho_position(dx,&new_dx);
 	//new_angle = deg2rad(new_angle);
 	teta = teta + m_rot;
+	printf("teta:%f\n", teta);
 	//teta = teta + new_angle
 	disp_sx = new_sx - old_sx; 
 	disp_dx = new_dx - old_dx;
+	printf("disp_sx:%f and disp_dx:%f\n",disp_sx,disp_dx);
 	disp_diff = (disp_sx + disp_dx)/2;
+	printf("dispdiff:%f\n",disp_diff);
 	old_sx = new_sx;
 	old_dx = new_dx;
 	delta_y = disp_diff * sin ( teta + m_rot/2);
 	delta_x = disp_diff * cos ( teta + m_rot/2);
+	printf("deltay:%f and deltax:%f\n",delta_y,delta_x);
 	//delta_y = disp_diff * sin ( teta + new_angle/2);
 	//delta_x = disp_diff * cos ( teta + new_angle/2);
 	old_y = old_y + delta_y;
