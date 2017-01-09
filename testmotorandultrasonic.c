08958c079449ced38c7fa65810127d45115c64a1
bassixt <coletta@eurecom.fr>
2017-01-09 18:19:48 +0100

    Update testmotorandultrasonic.c

--- testmotorandultrasonic.c
+++ testmotorandultrasonic.c
@@ -1353,7 +1353,7 @@ for(i=0;i<45;i++)
 		break;
 	}
 	//rotatesx(sn,dx,sn_compass,max_speed,1,sn_mag);
-	rotateforscan(sn,dx,max_speed);
+	rotateforscan(sn,dx,max_speed/2);
 	
 }
 //it has finished the search
