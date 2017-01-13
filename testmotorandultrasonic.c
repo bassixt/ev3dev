044d0e1e95d13511c2652a6255746df9eb2c2d17
bassixt <coletta@eurecom.fr>
2017-01-13 18:36:24 +0100

    Update testmotorandultrasonic.c

--- testmotorandultrasonic.c
+++ testmotorandultrasonic.c
@@ -626,7 +626,7 @@ return (finish-beginning)/21; //return the distance in cm
 void* positioning_sys(void* args)
 {	int seconds_bt=0;
  	char string[58];
- 	
+ 	int16_t x_conv,y_conv;
 	struct motandsens *donald = (struct motandsens *) args;	
 	while(1)
 	{
@@ -634,16 +634,17 @@ void* positioning_sys(void* args)
 	Sleep(100);
 	seconds_bt=seconds_bt+1;
 	if (seconds_bt == 20)
-	{	printf("provauint x:%d\n",(int16_t)donald->x);
-		//send position
-		printf("x: %f y: %f\n",donald->x,donald->y);
+	{	//send position
+	 	x_conv = ((int16_t)donald->x>>8) | ((int16_t)donald->x<<8);
+	 	y_conv = ((int16_t)donald->y>>8) | ((int16_t)donald->y<<8);
+		printf("x: %d y: %d\n",x_conv,y_conv);
 		*((uint16_t *) string) = msgId++;
 		string[2] = TEAM_ID;
 		string[3] = 0xFF;
 		string[4] = MSG_POSITION;
-		string[5] = (int)donald->x;          // x 
+		string[5] = x_conv;          // x 
 		string[6]= 0x00;
-		string[7] = (int)donald->y;	    // y 
+		string[7] = y_conv;	    // y 
 		string[8] = 0x00;
 		write(s, string, 9);
 		seconds_bt = 0;
