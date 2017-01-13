df4158829f6be5d47ff3b16d3882726e5a2e7e71
bassixt <coletta@eurecom.fr>
2017-01-13 18:14:40 +0100

    Update testmotorandultrasonic.c

--- testmotorandultrasonic.c
+++ testmotorandultrasonic.c
@@ -641,7 +641,7 @@ void* positioning_sys(void* args)
 		string[2] = TEAM_ID;
 		string[3] = 0xFF;
 		string[4] = MSG_POSITION;
-		string[5] = (int16)donald->x;          // x 
+		string[5] = (int16_t)donald->x;          // x 
 		string[6]= 0x00;
 		string[7] = (int16_t)donald->y;	    // y 
 		string[8] = 0x00;
