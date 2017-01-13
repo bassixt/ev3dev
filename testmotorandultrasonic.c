d3b7a48dfe451fdd2729b5019e5509a2517323c0
condex <francesco.condemi7@gmail.com>
2017-01-13 23:26:25 +0100

    Update testmotorandultrasonic.c

--- testmotorandultrasonic.c
+++ testmotorandultrasonic.c
@@ -1028,7 +1028,7 @@ switch(donald->number)
 
 		deltax=(donald->x)-xbefore;
 		deltay=(donald->y)-xbefore;
-		distanceback=sqrt(((donald->x)-xbefore)^2+((donald->x)-xbefore)^2)*19;
+		distanceback=sqrt(pow(((donald->x)-xbefore),2)+((donald->x)-xbefore)^2)*19;
 		angleofrotationback=atang(deltax/deltay)*180/M_PI;
 		printf("angle of turning back: %f",angleofrotationback);
 		rotatedx(donald->sn,donald->dx,donald->sn_compass,donald->max_speed,45,donald->sn_mag);
