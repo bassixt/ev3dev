d2895de12b61dc63e6339f6d9abde5a8813405e4
bassixt <coletta@eurecom.fr>
2017-01-14 00:37:17 +0100

    Update bt_messages.h

--- bt_messages.h
+++ bt_messages.h
@@ -21,12 +21,13 @@
 
 // UNIX //////////////////////////////////////////
 #else
-/*
+
 #include <unistd.h>
 #define Sleep( msec ) usleep(( msec ) * 1000 )
 
 //////////////////////////////////////////////////
 #endif
+
 const char const *color[] = { "?", "BLACK", "BLUE", "GREEN", "YELLOW", "RED", "WHITE", "BROWN" };
 #define COLOR_COUNT  (( int )( sizeof( color ) / sizeof( color[ 0 ])))
 
