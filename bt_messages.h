9bcc28948dfafbbc871c5945f51d61de19c68e44
bassixt <coletta@eurecom.fr>
2017-01-14 00:37:43 +0100

    Update bt_messages.h

--- bt_messages.h
+++ bt_messages.h
@@ -27,7 +27,7 @@
 
 //////////////////////////////////////////////////
 #endif
-
+/*
 const char const *color[] = { "?", "BLACK", "BLUE", "GREEN", "YELLOW", "RED", "WHITE", "BROWN" };
 #define COLOR_COUNT  (( int )( sizeof( color ) / sizeof( color[ 0 ])))
 
