4daee1fe0883f4d2df0ce3f09832e52ce7cfb45f
bassixt <coletta@eurecom.fr>
2017-01-13 11:24:20 +0100

    Update testmotorandultrasonic.c

--- testmotorandultrasonic.c
+++ testmotorandultrasonic.c
@@ -11,6 +11,9 @@
 #include "ev3_tacho.h"
 #include <math.h>
 #include "ev3_sensor.h"
+#include <sys/socket.h>
+#include <bluetooth/bluetooth.h>
+#include <bluetooth/rfcomm.h>
 // WIN32 /////////////////////////////////////////
 #ifdef __WIN32__
 
@@ -29,6 +32,28 @@ const char const *color[] = { "?", "BLACK", "BLUE", "GREEN", "YELLOW", "RED", "W
 
 #define MIN_STEP_VER 525 //minimum step covered going ahead in cm (25cm)
 pthread_mutex_t mutex,mutex_pos;
+
+//////////////////////////////////
+//         FOR BT               //
+//////////////////////////////////
+#define SERV_ADDR   "00:1E:10:00:06:2B"     /* address of the server is */
+#define TEAM_ID     10                       /* team ID */
+
+#define MSG_ACK     0
+#define MSG_NEXT    1
+#define MSG_START   2
+#define MSG_STOP    3
+#define MSG_CUSTOM  4
+#define MSG_KICK    5
+#define MSG_POSITION 6
+#define MSG_BALL 	7
+unsigned char rank = 0;
+unsigned char length = 0;
+unsigned char previous = 0xFF;
+unsigned char next = 0xFF;
+unsigned char side=0;
+int s;
+
 
 typedef struct motandsens test;
 struct motandsens {
@@ -55,6 +80,33 @@ struct pos {
 	float x,y
 	};
 //calculate the the condition for the loop in the next function
+
+
+////////////////////////////////////////////////////////////
+/////			BT FUNCTIONS			////
+/////			   START			////
+////////////////////////////////////////////////////////////
+int read_from_server (int sock, char *buffer, size_t maxSize) {
+    int bytes_read = read (sock, buffer, maxSize);
+
+    if (bytes_read <= 0) {
+        fprintf (stderr, "Server unexpectedly closed connection...\n");
+        close (s);
+        exit (EXIT_FAILURE);
+    }
+
+    printf ("[DEBUG] received %d bytes\n", bytes_read);
+
+    return bytes_read;
+}
+////////////////////////////////////////////////////////////
+/////			BT FUNCTIONS			////
+/////			   FINISH			////
+////////////////////////////////////////////////////////////
+
+
+
+
 int getcondition(int role, int arena, int side,float real_posx, float real_posy)
 {
 	int res_cond;
@@ -1414,20 +1466,75 @@ int main( int argc, char **argv )
         while ( ev3_tacho_init() < 1 ) Sleep( 1000 );
 
  
- /* Lecture of the executions parameters */
-  /*if ((argc < 3) || (argc>4)){
-    fputs("USAGE : run <caseNumber> \n",
-	  stderr);
-    return EXIT_FAILURE;
-  }*/
+
  
-  caseNumber = atoi(argv[1]);
+ 	caseNumber = atoi(argv[1]);
  
         printf( "*** ( EV3 ) Hello! ***\n" );
 	
 	donald = inizialization(donald);
  	donald->number = caseNumber;
- 
+ 	///////////////////////////////////////////////////////////////////////
+	////								   ////
+	////			CONNECTION TO SERVER			   ////
+	////								   ////
+	///////////////////////////////////////////////////////////////////////
+    struct sockaddr_rc addr = { 0 };
+    int status;
+    
+    /* allocate a socket */
+    s = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
+
+    /* set the connection parameters (who to connect to) */
+    addr.rc_family = AF_BLUETOOTH;
+    addr.rc_channel = (uint8_t) 1;
+    str2ba (SERV_ADDR, &addr.rc_bdaddr);
+
+    /* connect to server */
+    status = connect(s, (struct sockaddr *)&addr, sizeof(addr));
+
+    /* if connected */
+    if( status == 0 ) {
+        char string[58];
+
+        /* Wait for START message */
+        read_from_server (s, string, 9);
+        if (string[4] == MSG_START) {
+            printf ("Received start message!\n");
+            rank = (unsigned char) string[5];
+	    side = (unsigned char) string[6];
+            next = (unsigned char) string[7];
+			
+        }
+		if (side==0){
+		    printf("I am on the right side\n");
+		}
+		else {
+			printf("I am on the left side\n");
+		}
+		
+        if (rank == 0){
+            printf("beginner\n");
+	    }
+        else
+            printf("beginner\n");
+
+
+
+    } else {
+        fprintf (stderr, "Failed to connect to server...\n");
+        sleep (2);
+	close(s);
+        exit (EXIT_FAILURE);
+    }
+
+   
+	///////////////////////////////////////////////////////////////////////
+	////								   ////
+	////			CONNECTION ESTABLISHED			   ////
+	////								   ////
+	///////////////////////////////////////////////////////////////////////
+
 	retour = pthread_create(&thread_movement, NULL, movements, donald);
 	if (retour != 0)
 	{
