114a66dd56f14cbaab0f9de1068f87107e7bf00f
bassixt <coletta@eurecom.fr>
2017-01-17 17:16:27 +0100

    Update testmotorandultrasonic.c

--- testmotorandultrasonic.c
+++ testmotorandultrasonic.c
@@ -798,8 +798,8 @@ if ( !get_sensor_value0(donald->sn_mag, &heading)){
 switch(donald->number)
 {
 	case 0 :
-		//gotoxyfinisher(donald->x, donald->y, 20.0, 35.0,donald->sn,donald->dx,donald->max_speed,donald->sn_sonar, donald->sn_compass, donald->sn_mag);	
-		 research(donald->sn,donald->dx, donald->max_speed, donald->sn_compass, 30 , donald->med, donald->sn_color, donald->sn_mag, donald->sn_sonar);
+		gotoxyfinisher(donald->x, donald->y, 20.0, 35.0,donald->sn,donald->dx,donald->max_speed,donald->sn_sonar, donald->sn_compass, donald->sn_mag);	
+		research(donald->sn,donald->dx, donald->max_speed, donald->sn_compass, 30 , donald->med, donald->sn_color, donald->sn_mag, donald->sn_sonar);
 		//gotoxybeg(donald->x, donald->y, 20.0, 35.0,donald->sn,donald->dx,donald->max_speed,donald->sn_sonar, donald->sn_compass, donald->sn_mag);
 		break;
 	case 1 :
