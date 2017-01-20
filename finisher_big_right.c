9e9990acf5a4ad5f30ab8aee87c0df672f012e13
bassixt <coletta@eurecom.fr>
2017-01-20 21:20:41 +0100

    Update finisher_big_right.c

--- finisher_big_right.c
+++ finisher_big_right.c
@@ -813,14 +813,15 @@ switch(donald->number)
 	case 0 ://prima era x=19 y=43 e 1 e 150
 		//waitning for the next
 		Sleep(1000);
-		gotoxybeg(donald->x, donald->y, 38.0, 225.0,donald->sn,donald->dx,donald->max_speed,donald->sn_sonar, donald->sn_compass, donald->sn_mag, donald->teta);	
+		research2(donald->sn,donald->dx, donald->max_speed, donald->sn_compass, 25 , donald->med, donald->sn_color, donald->sn_mag, donald->sn_sonar);
+		//gotoxybeg(donald->x, donald->y, 38.0, 225.0,donald->sn,donald->dx,donald->max_speed,donald->sn_sonar, donald->sn_compass, donald->sn_mag, donald->teta);	
 		//gotoxybeg(donald->x, donald->y, 38.0, 192.0,donald->sn,donald->dx,donald->max_speed,donald->sn_sonar, donald->sn_compass, donald->sn_mag, donald->teta);
 		//rotatesx(donald->sn,donald->dx,donald->max_speed,180,donald->sn_mag);
 		//leave_ball(donald->sn,donald->dx,donald->med,donald->max_speed);
 		//go_backward(donald->sn,donald->dx,donald->med,donald->max_speed);
 		
-		put_down(donald->med,donald->max_speed);
-		gotoxybeg(donald->x, donald->y, 15, 350.0,donald->sn,donald->dx,donald->max_speed,donald->sn_sonar, donald->sn_compass, donald->sn_mag, donald->teta);
+		//put_down(donald->med,donald->max_speed);
+		//gotoxybeg(donald->x, donald->y, 15, 350.0,donald->sn,donald->dx,donald->max_speed,donald->sn_sonar, donald->sn_compass, donald->sn_mag, donald->teta);
 		//rotatedx of 180
 		//leave the ball
 		//send ball position
