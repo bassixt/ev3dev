e175d5b57ec6a9e2c332775a0bed4e2f542c0031
bassixt <coletta@eurecom.fr>
2017-01-20 20:21:16 +0100

    Update beginner_big_right.c

--- beginner_big_right.c
+++ beginner_big_right.c
@@ -815,7 +815,7 @@ switch(donald->number)
 		Sleep(1000);
 		gotoxybeg(donald->x, donald->y, 72.0, 140.0,donald->sn,donald->dx,donald->max_speed,donald->sn_sonar, donald->sn_compass, donald->sn_mag, donald->teta);	
 		gotoxybeg(donald->x, donald->y, 40.0, 200.0,donald->sn,donald->dx,donald->max_speed,donald->sn_sonar, donald->sn_compass, donald->sn_mag, donald->teta);
-		rotatedsx(donald->sn,donald->dx,donald->max_speed,180,donald->sn_mag);
+		rotatesx(donald->sn,donald->dx,donald->max_speed,180,donald->sn_mag);
 		leave_ball(donald->sn,donald->dx,donald->med,donald->max_speed);
 		go_backward(donald->sn,donald->dx,donald->med,donald->max_speed);
 		go_backward(donald->sn,donald->dx,donald->med,donald->max_speed);
