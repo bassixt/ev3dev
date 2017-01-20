a9166ab5925eb51f54f56e7b54ce165b71053f41
bassixt <coletta@eurecom.fr>
2017-01-20 21:35:25 +0100

    Update finisher_big_right.c

--- finisher_big_right.c
+++ finisher_big_right.c
@@ -813,8 +813,9 @@ switch(donald->number)
 	case 0 ://prima era x=19 y=43 e 1 e 150
 		//waitning for the next
 		Sleep(1000);
+		//research2(donald->sn,donald->dx, donald->max_speed, donald->sn_compass, 25 , donald->med, donald->sn_color, donald->sn_mag, donald->sn_sonar);
+		gotoxybeg(donald->x, donald->y, 38.0, 225.0,donald->sn,donald->dx,donald->max_speed,donald->sn_sonar, donald->sn_compass, donald->sn_mag, donald->teta);	
 		research2(donald->sn,donald->dx, donald->max_speed, donald->sn_compass, 25 , donald->med, donald->sn_color, donald->sn_mag, donald->sn_sonar);
-		//gotoxybeg(donald->x, donald->y, 38.0, 225.0,donald->sn,donald->dx,donald->max_speed,donald->sn_sonar, donald->sn_compass, donald->sn_mag, donald->teta);	
 		//gotoxybeg(donald->x, donald->y, 38.0, 192.0,donald->sn,donald->dx,donald->max_speed,donald->sn_sonar, donald->sn_compass, donald->sn_mag, donald->teta);
 		//rotatesx(donald->sn,donald->dx,donald->max_speed,180,donald->sn_mag);
 		//leave_ball(donald->sn,donald->dx,donald->med,donald->max_speed);
