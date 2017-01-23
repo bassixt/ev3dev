d51244c38f2176a701b457ddd009d439e7bb235f
bassixt <coletta@eurecom.fr>
2017-01-23 18:12:18 +0100

    Update finisher_big_left.c

--- finisher_big_left.c
+++ finisher_big_left.c
@@ -584,11 +584,11 @@ int retour;
 float value;
 float initial_angle;
 set_tacho_time_sp( sn, 200 );
-set_tacho_ramp_up_sp( sn, 1000 );
-set_tacho_ramp_down_sp( sn, 1000);
+set_tacho_ramp_up_sp( sn, 2000 );
+set_tacho_ramp_down_sp( sn, 2000);
 set_tacho_time_sp( dx, 200 );
-set_tacho_ramp_up_sp( dx, 1000 );
-set_tacho_ramp_down_sp( dx, 1000 );
+set_tacho_ramp_up_sp( dx, 2000 );
+set_tacho_ramp_down_sp( dx, 2000 );
 get_tacho_position( dx, &beginning);
 finish = beginning;
 if ( !get_sensor_value0(sn_mag, &initial_angle)){
@@ -603,11 +603,11 @@ while((finish - beginning - distance)<=0){
 	multi_set_tacho_ramp_down_sp( both, 2000 );
 	*/
 	set_tacho_time_sp( sn, 200 );
-	set_tacho_ramp_up_sp( sn, 1500 );
-	set_tacho_ramp_down_sp( sn, 1500 );
+	set_tacho_ramp_up_sp( sn, 2000 );
+	set_tacho_ramp_down_sp( sn, 2000 );
 	set_tacho_time_sp( dx, 200);
-	set_tacho_ramp_up_sp( dx, 1500 );
-	set_tacho_ramp_down_sp( dx, 1500 );
+	set_tacho_ramp_up_sp( dx, 2000 );
+	set_tacho_ramp_down_sp( dx, 2000 );
 	/*retour = pthread_mutex_lock(&mutex);
     			if (retour != 0)
     			 {
